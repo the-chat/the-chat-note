0ea5246c3935f4da61be10e8a669aaf7e1399c56
the-chat <thechatllc@gmail.com>
2022-03-20 16:17:00 +0500

    next i18next configs

---
+++ next-i18next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "ru"],
+  },
+  react: {
+    useSuspense: false,
+  },
+}
---
+++ next.config.js
@@ -0,0 +1,3 @@
+/* eslint @typescript-eslint/no-var-requires: 0 */
+
+module.exports = { i18n: require("./next-i18next.config").i18n }

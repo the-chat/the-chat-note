b33372e0f5de90b8513fdbe34fd8d3f74bc9ce6c
the-chat <thechatllc@gmail.com>
2022-03-20 15:18:43 +0500

    index

---
+++ pages/index.tsx
@@ -0,0 +1,6 @@
+import nextJSPropsWithTranslation from "utils/nextJSPropsWithTranslation"
+
+const Index = () => <>HEY</>
+
+export default Index
+export const getStaticProps = nextJSPropsWithTranslation()

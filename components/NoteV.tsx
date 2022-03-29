6b0ef968caca90d7849811bf18f1c407bc842950
the-chat <thechatllc@gmail.com>
2022-03-29 13:56:33 +0500

    NoteV

---
+++ components/NoteV.tsx
@@ -0,0 +1,37 @@
+import * as React from "react"
+import Card from "@mui/material/Card"
+import CardContent from "@mui/material/CardContent"
+import CardMedia from "@mui/material/CardMedia"
+import Typography from "@mui/material/Typography"
+import { Grid,IconButton, Button, CardActionArea, CardActions } from "@mui/material"
+import { Note } from "types"
+import { Link } from "@the-chat/ui-kit"
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+// todo: img next
+const NoteV = ({ id, title, text, photoSrc }: Note) => (
+  <Grid item>
+    <Card>
+      <Link href={"/note/" + id}>
+        <CardActionArea>
+          {photoSrc && <CardMedia component="img" image={photoSrc} />}
+          <CardContent>
+            <Typography gutterBottom={text} variant="h5">
+              <textarea>{title}</textarea>
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {text}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Link>
+       <CardActions>
+       <IconButton>
+       <MoreVertIcon/>
+       </IconButton>
+    </CardActions> 
+    </Card>
+  </Grid>
+)
+
+export default NoteV

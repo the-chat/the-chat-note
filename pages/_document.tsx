37de59f210999abce1685179ee3f5eb60566b5c0
the-chat <thechatllc@gmail.com>
2022-03-29 12:20:22 +0500

    serverside in document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,8 @@
 import Document, { Head, Html, Main, NextScript } from "next/document"
 import { defaultProps, DocumentHead } from "@the-chat/ui-kit"
 import manifest from "../public/manifest.json"
+import { ServerStyleSheets } from "@mui/styles"
+import { Children } from "react"
 
 // learn: Dcoument to func
 // learn: should i translate all this metatags
@@ -27,5 +29,28 @@ export default class MyDocument extends Document {
         </body>
       </Html>
     )
+  }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  // learn
+  // Render app and page and get the context of the page with collected side effects.
+  const sheets = new ServerStyleSheets()
+  const originalRenderPage = ctx.renderPage
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    })
+
+  const initialProps = await Document.getInitialProps(ctx)
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [
+      ...Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
   }
 }

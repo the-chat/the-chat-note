1f5fceb2c3d708d3cd6556d137e25aa97d858dde
the-chat <thechatllc@gmail.com>
2022-03-29 13:57:10 +0500

    notes list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,20 @@
 import NewNote from "components/NewNote"
+import NoteV from "components/NoteV"
 import nextJSPropsWithTranslation from "utils/nextJSPropsWithTranslation"
 import useNotes from "utils/useNotes"
+import { Grid } from "@mui/material"
 
 const Index = () => {
   const notes = useNotes()
 
   return (
     <>
-      Your notes!
-      {notes.map((note) => note.text)}
       <NewNote />
+      <Grid spacing={2} container>
+        {notes.map((note) => (
+          <NoteV key={note.id} {...note} />
+        ))}
+      </Grid>
     </>
   )
 }

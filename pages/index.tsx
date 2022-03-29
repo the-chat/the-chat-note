76c88679329e13f1dae3393acbbdd68910677ba9
the-chat <thechatllc@gmail.com>
2022-03-29 12:20:36 +0500

    index v0.1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,18 @@
+import NewNote from "components/NewNote"
 import nextJSPropsWithTranslation from "utils/nextJSPropsWithTranslation"
+import useNotes from "utils/useNotes"
 
-const Index = () => <>HEY</>
+const Index = () => {
+  const notes = useNotes()
+
+  return (
+    <>
+      Your notes!
+      {notes.map((note) => note.text)}
+      <NewNote />
+    </>
+  )
+}
 
 export default Index
 export const getStaticProps = nextJSPropsWithTranslation()

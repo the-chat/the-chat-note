4f9a0116978ce09914ca86cef717dd06767a08a0
the-chat <thechatllc@gmail.com>
2022-04-11 21:22:05 +0500

    no using of placeholder, placeholder in it's own file

---
+++ utils/notesPlaceholder.ts
@@ -0,0 +1,50 @@
+import { Note } from "types"
+
+const onlyText = {
+  id: "1",
+  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tempus iaculis urna id volutpat lacus laoreet non curabitur. In ante metus dictum at tempor commodo. Sit amet aliquam id diam maecenas ultricies mi eget. Consectetur adipiscing elit pellentesque habitant morbi.",
+}
+
+const textAndTitle = {
+  id: "2",
+  title: "Lorem ipsum dolor sit amet",
+  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tempus iaculis urna id volutpat lacus laoreet non curabitur. In ante metus dictum at tempor commodo. Sit amet aliquam id diam maecenas ultricies mi eget. Consectetur adipiscing elit pellentesque habitant morbi.",
+}
+
+const title = {
+  id: "3",
+  title: "Lorem ipsum dolor sit amet",
+}
+
+const photoAndText = {
+  id: "4",
+  photoSrc: "https://the-chat-resources.vercel.app/photos/the-chat-logo.png",
+  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tempus iaculis urna id volutpat lacus laoreet non curabitur. In ante metus dictum at tempor commodo. Sit amet aliquam id diam maecenas ultricies mi eget. Consectetur adipiscing elit pellentesque habitant morbi.",
+}
+
+const photoTextTitle = {
+  id: "5",
+  photoSrc:
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.zTo7cffseGhNkheg80calAHaEK%26pid%3DApi&f=1",
+  title: "Lorem ipsum dolor sit amet",
+  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tempus iaculis urna id volutpat lacus laoreet non curabitur. In ante metus dictum at tempor commodo. Sit amet aliquam id diam maecenas ultricies mi eget. Consectetur adipiscing elit pellentesque habitant morbi.",
+}
+
+const vertPhotoTextTitle = {
+  id: "6",
+  photoSrc:
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Lul3BkD1C0ysD3pY6ApmvQHaHa%26pid%3DApi&f=1",
+  title: "Lorem ipsum dolor sit amet",
+  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tempus iaculis urna id volutpat lacus laoreet non curabitur. In ante metus dictum at tempor commodo. Sit amet aliquam id diam maecenas ultricies mi eget. Consectetur adipiscing elit pellentesque habitant morbi.",
+}
+
+const placeholder: Note[] = [
+  onlyText,
+  title,
+  photoAndText,
+  photoTextTitle,
+  textAndTitle,
+  vertPhotoTextTitle,
+]
+
+export default placeholder
--- utils/useNotes.ts
+++ utils/useNotes.ts
@@ -1,60 +1,13 @@
 import { useUser } from "@the-chat/use-user"
 import { Note } from "types"
 
-const onlyText = {
-  id: "1",
-  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tempus iaculis urna id volutpat lacus laoreet non curabitur. In ante metus dictum at tempor commodo. Sit amet aliquam id diam maecenas ultricies mi eget. Consectetur adipiscing elit pellentesque habitant morbi.",
-}
-
-const textAndTitle = {
-  id: "2",
-  title: "Lorem ipsum dolor sit amet",
-  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tempus iaculis urna id volutpat lacus laoreet non curabitur. In ante metus dictum at tempor commodo. Sit amet aliquam id diam maecenas ultricies mi eget. Consectetur adipiscing elit pellentesque habitant morbi.",
-}
-
-const title = {
-  id: "3",
-  title: "Lorem ipsum dolor sit amet",
-}
-
-const photoAndText = {
-  id: "4",
-  photoSrc: "https://the-chat-resources.vercel.app/photos/the-chat-logo.png",
-  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tempus iaculis urna id volutpat lacus laoreet non curabitur. In ante metus dictum at tempor commodo. Sit amet aliquam id diam maecenas ultricies mi eget. Consectetur adipiscing elit pellentesque habitant morbi.",
-}
-
-const photoTextTitle = {
-  id: "5",
-  photoSrc:
-    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.zTo7cffseGhNkheg80calAHaEK%26pid%3DApi&f=1",
-  title: "Lorem ipsum dolor sit amet",
-  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tempus iaculis urna id volutpat lacus laoreet non curabitur. In ante metus dictum at tempor commodo. Sit amet aliquam id diam maecenas ultricies mi eget. Consectetur adipiscing elit pellentesque habitant morbi.",
-}
-
-const vertPhotoTextTitle = {
-  id: "6",
-  photoSrc:
-    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Lul3BkD1C0ysD3pY6ApmvQHaHa%26pid%3DApi&f=1",
-  title: "Lorem ipsum dolor sit amet",
-  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tempus iaculis urna id volutpat lacus laoreet non curabitur. In ante metus dictum at tempor commodo. Sit amet aliquam id diam maecenas ultricies mi eget. Consectetur adipiscing elit pellentesque habitant morbi.",
-}
-
-const placeholder: Note[] = [
-  onlyText,
-  title,
-  photoAndText,
-  photoTextTitle,
-  textAndTitle,
-  vertPhotoTextTitle,
-]
-
 function useNotes(): Note[]
 function useNotes(id: string): Note
 function useNotes(id?: string) {
-  const [{ notes }] = useUser()
+  const [{ notes = [] }] = useUser()
 
-  if (id) return placeholder.filter((x) => x.id == id)[0]
-  return placeholder
+  if (id) return notes.filter((x) => x.id == id)[0]
+  return notes
 }
 
 export default useNotes

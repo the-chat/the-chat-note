d619f479e42a61368d7f6246289d204194d50d25
the-chat <thechatllc@gmail.com>
2022-04-13 18:40:21 +0500

    marksConfig: Marks, getOnClick

--- config.ts
+++ config.ts
@@ -1,20 +1,29 @@
 import EditLocationIcon from "@mui/icons-material/EditLocation"
 import TagIcon from "@mui/icons-material/Tag"
 import { SvgIconComponent } from "@mui/icons-material"
+import { SetState } from "@the-chat/types"
+import { Marks } from "types"
 
 export const marksConfig: Record<
-  string,
+  Marks,
   {
     DefaultIcon: SvgIconComponent
     text: string
+    getOnClick: (setOpen: SetState<boolean>) => () => void
   }
 > = {
   location: {
     DefaultIcon: EditLocationIcon,
-    text: "",
+    text: "Choose Location",
+    getOnClick: (setOpen) => () => {
+      setOpen(true)
+    },
   },
   tag: {
     DefaultIcon: TagIcon,
-    text: "",
+    text: "Choose Tag",
+    getOnClick: (setOpen) => () => {
+      setOpen(true)
+    },
   },
 }
